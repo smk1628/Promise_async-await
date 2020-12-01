eef1e90fc8625eeb10e8fe1eb9179001839744cc
smk1628 <1628150422@qq.com>
2020-12-01 08:37:43 +0800

    update

---
+++ promise.js
@@ -0,0 +1,15 @@
+//使用promise
+new Promise((resolve,reject)=>{
+    //一段耗时的promise操作 
+    resolve('success')
+}).then(
+    (res)=>{console.log(res)}, //成功
+    (err)=>{console.log(err)} //失败
+)
+
+//promise api
+
+Promise.all()
+Promise.then()
+Promise.catch()
+Promise.race()
